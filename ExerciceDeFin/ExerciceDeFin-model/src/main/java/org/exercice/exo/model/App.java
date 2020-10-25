39412a285fc381f59d937d0a7416ed97937babed
Sebastien Fremy <sebastien.fremy1986@gmail.com>
2020-10-25 22:11:41 +0100

    Initilisation de la class main pour le module batch lors de la génération du jar

---
+++ ExerciceDeFin/ExerciceDeFin-model/src/main/java/org/exercice/exo/model/App.java
@@ -0,0 +1,13 @@
+package org.exercice.exo.model;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
